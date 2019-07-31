6d1adf1e5c070980c48519d827d1e239ed2ab481
Miguel <2029ad@gmail.com>
2019-07-31 16:47:22 +0200

    start layouts

--- shareyours_app/src/components/Navbar.tsx
+++ shareyours_app/src/components/Navbar.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 
-
-
 const Navbar: React.FC = () => {
-    const { Navbar, NavItem } = require("react-materialize");
+  // const { Navbar, NavItem } = require("react-materialize");
   return (
-    <Navbar brand={<a />} alignLinks="right">
-      <NavItem href="">Getting started</NavItem>
-      <NavItem href="components.html">Components</NavItem>
-    </Navbar>
+    <nav>
+      <div className="nav-wrapper deep-purple lighten-4">
+        <a href="#" className="brand-logo">
+          Logo
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">login</a>
+          </li>
+          <li>
+            <a href="badges.html">registrarse</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
--- shareyours_app/src/layouts/LayoutPage.tsx
+++ shareyours_app/src/layouts/LayoutPage.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
-
-
-const LayoutPage: React.FC = () =>{
-    return(
-        <div>fddffd</div>
-    )
-}
+const LayoutPage: React.FC = () => {
+  return (
+    <div className="row">
+      <div className="col s12">
+          <Navbar></Navbar>
+      </div>
+    </div>
+  );
+};
 
 export default LayoutPage;
